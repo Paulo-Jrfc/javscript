417fdf7a2a27a8c499d6e4293cdd2740d46c4361
Paulo Junior <103224304+Paulo-Jrfc@users.noreply.github.com>
2022-11-18 17:31:36 -0300

    Create ex011.js

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 18
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18){
+        console.log('Voto opciomal')
+} else{
+    console.log('Vota obrigatório')
+}
